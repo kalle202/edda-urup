e2ad6d2d0b8091dc608fff07f1629ad32e8cedc9
[Kjartan Roberts] <[kjartanuroberts@gmail.com]>
2023-04-19 10:11:15 +0200

    ignore build errors in next.config.js

--- store.ts
+++ store.ts
@@ -1,6 +1,6 @@
-import { create } from "zustand";
-import { persist } from "zustand/middleware";
-import { AddCartType } from "./types/AddCartType";
+import { create } from "zustand"
+import { persist } from "zustand/middleware"
+import { AddCartType } from "./types/AddCartType"
 
 type CartState = {
     isOpen: boolean
